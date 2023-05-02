4faf65609023a50a30517958ce5245f734c9a037
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-02 23:11:53 +0800

    Format, add enum and remove header definition

--- main.c
+++ main.c
@@ -1,167 +1,195 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    void *next;
-    int data;
+enum Action
+{
+  Add = 1,
+  Remove,
+  Insert,
+  Print,
+};
+
+typedef struct
+{
+  void *next;
+  int data;
+} Test;
+
+typedef struct
+{
+  void *next;
+  int data;
 } Node;
 
 Node *head = NULL;
 
-Node *addNode(int data);
+Node *insertNode(int data, int position)
+{
+  Node *current = head;
+  while (current != NULL && position != 0)
+  {
+    position--;
+  }
 
-Node *insertNode(int data, int position);
+  if (position != 0)
+  {
+    printf("Requested position too far into list\n");
+    return NULL;
+  }
 
-void printMenu();
+  Node *new = malloc(sizeof(Node));
+  if (new == NULL)
+  {
+    return NULL;
+  }
 
-void printList();
+  new->data = data;
+  new->next = current->next;
+  current->next = new;
 
-int removeNode(int data);
-
-
-int main(int argc, char **argv) {
-    int option = -1;
-    int arg1 = 0;
-    int arg2 = 0;
-
-    while (option != 5) {
-        printMenu();
-
-        int num_received = scanf("%d", &option);
-        if (num_received == 1 && option > 0 && option <= 5) {
-            switch (option) {
-                case 1:
-                    // Add operation
-                    setbuf(stdout, 0);
-                    printf("What data should I insert?:\n");
-                    scanf("%d", &arg1);
-                    Node *new = addNode(arg1);
-                    break;
-                case 2:
-                    // Remove Operation
-                    printf("What data should I remove?:\n");
-                    scanf("%d", &arg1);
-                    int success = removeNode(arg1);
-                    if (!success) {
-                        printf("Element not found\n");
-                    }
-                    break;
-                case 3:
-                    // Insert operation
-                    printf("What data should I insert?:\n");
-                    scanf("%d", &arg1);
-                    printf("What position?:\n");
-                    scanf("%d", &arg2);
-                    new = insertNode(arg1, arg2);
-                    if (new == NULL) {
-                        printf("Failed to insert into list\n");
-                    }
-                    break;
-                case 4:
-                    printList();
-                    break;
-                case 5:
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    return 0;
+  return new;
 }
 
-Node *insertNode(int data, int position) {
-    Node *current = head;
-    while (current != NULL && position != 0) {
-        position--;
-    }
+int removeNode(int data)
+{
+  Node *current = head;
+  Node *prev = head;
 
-    if (position != 0) {
-        printf("Requested position too far into list\n");
-        return NULL;
+  while (current != NULL)
+  {
+    if (current->data == data)
+    {
+      // If current node is the list head
+      if (current == head)
+      {
+        head = current->next;
+        free(current);
+        current = NULL;
+      }
+      else
+      {
+        prev->next = current->next;
+        free(current);
+        current = NULL;
+      }
+      return 1;
     }
+    prev = current;
+    current = current->next;
+  }
+  return 0;
+}
 
-    Node *new = malloc(sizeof(Node));
-    if (new == NULL) {
-        return NULL;
-    }
+Node *addNode(int data)
+{
+  Node *new = NULL;
+  // Two cases:
+  // If the list is empty.
+  if (head == NULL)
+  {
+    new = malloc(sizeof(Node));
+    if (new == NULL)
+      return NULL;
 
     new->data = data;
-    new->next = current->next;
-    current->next = new;
+    head = new;
+    new->next = NULL;
+  }
+  else
+  {
+    new = malloc(sizeof(Node));
+    if (new == NULL)
+      return NULL;
 
-    return new;
+    new->data = data;
+    new->next = head;
+    head = new;
+  }
+
+  return NULL;
 }
 
-int removeNode(int data) {
-    Node *current = head;
-    Node *prev = head;
-
-    while (current != NULL) {
-        if (current->data == data) {
-            // If current node is the list head
-            if (current == head) {
-                head = current->next;
-                free(current);
-                current = NULL;
-            } else {
-                prev->next = current->next;
-                free(current);
-                current = NULL;
-            }
-            return 1;
-        }
-        prev = current;
-        current = current->next;
-    }
-    return 0;
+void printMenu()
+{
+  setbuf(stdout, 0);
+  printf("You have the following options:\n");
+  printf("\t1. Add a node to the list.\n");
+  printf("\t2. Remove a node from the list.\n");
+  printf("\t3. Insert a node to the list.\n");
+  printf("\t4. Print your list\n");
+  printf("\t5. Quit.\n");
+  return;
 }
 
-Node *addNode(int data) {
-    Node *new = NULL;
-    // Two cases:
-    // If the list is empty.
-    if (head == NULL) {
-        new = malloc(sizeof(Node));
-        if (new == NULL) return NULL;
-
-        new->data = data;
-        head = new;
-        new->next = NULL;
-    } else {
-        new = malloc(sizeof(Node));
-        if (new == NULL)
-            return NULL;
-
-        new->data = data;
-        new->next = head;
-        head = new;
-    }
-
-    return NULL;
-}
-
-void printMenu() {
+void printList()
+{
+  Node *current = head;
+  while (current != NULL)
+  {
     setbuf(stdout, 0);
-    printf("You have the following options:\n");
-    printf("\t1. Add a node to the list.\n");
-    printf("\t2. Remove a node from the list.\n");
-    printf("\t3. Insert a node to the list.\n");
-    printf("\t4. Print your list\n");
-    printf("\t5. Quit.\n");
-    return;
+    printf("%d->", current->data);
+    current = current->next;
+  }
+
+  setbuf(stdout, 0);
+  printf("\n");
+  return;
 }
 
-void printList() {
-    Node *current = head;
-    while (current != NULL) {
+int main(int argc, char **argv)
+{
+  int option = -1;
+  int arg1 = 0;
+  int arg2 = 0;
+
+  while (option != 5)
+  {
+    printMenu();
+
+    int num_received = scanf("%d", &option);
+    if (num_received == 1 && option > 0 && option <= 5)
+    {
+      switch (option)
+      {
+      case Add:
+        // Add operation
         setbuf(stdout, 0);
-        printf("%d->", current->data);
-        current = current->next;
+        printf("What data should I insert?:\n");
+        scanf("%d", &arg1);
+        Node *new = addNode(arg1);
+        break;
+      case Remove:
+        // Remove Operation
+        printf("What data should I remove?:\n");
+        scanf("%d", &arg1);
+        int success = removeNode(arg1);
+        if (!success)
+        {
+          printf("Element not found\n");
+        }
+        break;
+      case Insert:
+        // Insert operation
+        printf("What data should I insert?:\n");
+        scanf("%d", &arg1);
+        printf("What position?:\n");
+        scanf("%d", &arg2);
+        new = insertNode(arg1, arg2);
+        if (new == NULL)
+        {
+          printf("Failed to insert into list\n");
+        }
+        break;
+      case Print:
+        printList();
+        break;
+      case 5:
+        break;
+      default:
+        break;
+      }
     }
+  }
 
-    setbuf(stdout, 0);
-    printf("\n");
-    return;
+  return 0;
 }
